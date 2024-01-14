83515c2310afc691c0768b2a8a6f1b4fd21313ff
Timo Kössler <info@timokoessler.de>
2024-01-14 17:33:13 +0100

    Add tests for private registries

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -2,35 +2,36 @@ import chalk from 'chalk';
 import { isDev, isTest } from './helpers';
 
 export function log(level: 'error' | 'warn' | 'info' | 'debug', content: string | object) {
+    /* istanbul ignore else  */
     if (isTest()) {
-        if (level === 'error') {
+        if (level === 'error' && process.env.EXPECT_ERROR !== 'true') {
             throw new Error(content.toString());
         }
-        return;
-    }
-    let txt: string;
-    if (typeof content === 'string') {
-        txt = content;
     } else {
-        try {
-            txt = JSON.stringify(content);
-        } catch (error) {
-            txt = content.toString();
+        let txt: string;
+        if (typeof content === 'string') {
+            txt = content;
+        } else {
+            try {
+                txt = JSON.stringify(content);
+            } catch (error) {
+                txt = content.toString();
+            }
         }
-    }
 
-    // Remove newline characters to protect against crlf injection
-    txt = txt.replace(/(\r\n|\n|\r)/gm, '');
+        // Remove newline characters to protect against crlf injection
+        txt = txt.replace(/(\r\n|\n|\r)/gm, '');
 
-    const dateStr = new Date().toISOString();
+        const dateStr = new Date().toISOString();
 
-    if (level === 'error') {
-        console.error(`${dateStr} - ${chalk.bgRed('ERROR')}: ${txt}`);
-    } else if (level === 'warn') {
-        console.warn(`${dateStr} - ${chalk.yellow('WARN')}: ${txt}`);
-    } else if (level === 'info') {
-        console.info(`${dateStr} - ${chalk.blue('INFO')}: ${txt}`);
-    } else if (isDev()) {
-        console.log(`${dateStr} - DEBUG: ${txt}`);
+        if (level === 'error') {
+            console.error(`${dateStr} - ${chalk.bgRed('ERROR')}: ${txt}`);
+        } else if (level === 'warn') {
+            console.warn(`${dateStr} - ${chalk.yellow('WARN')}: ${txt}`);
+        } else if (level === 'info') {
+            console.info(`${dateStr} - ${chalk.blue('INFO')}: ${txt}`);
+        } else if (isDev()) {
+            console.log(`${dateStr} - DEBUG: ${txt}`);
+        }
     }
 }
---
+++ tests/private-registry.test.ts
@@ -0,0 +1,54 @@
+import { initServer } from './test-helpers';
+import { getContainerRegistryAuth, getContainerRegistryHost, pullImage } from '../src/core/docker';
+
+process.env.NODE_ENV = 'TEST';
+process.env.PORT = '3000';
+process.env.IP = '127.0.0.1';
+
+beforeAll(async () => {
+    await initServer();
+});
+
+test('Get container registry host', () => {
+    const host = getContainerRegistryHost('test/test:latest');
+    expect(host).toEqual('docker.io');
+});
+
+test('Get container registry auth for registry.example.com', async () => {
+    const auth = await getContainerRegistryAuth('registry.example.com/test/test:latest');
+
+    expect(auth.username).toEqual('docker');
+    expect(auth.password).toEqual('secretpassword');
+});
+
+test('Get non-existing container registry auth', async () => {
+    const auth = await getContainerRegistryAuth('registry.example.com:5000/test/test:latest');
+    expect(auth).toBeUndefined();
+});
+
+test('Get container registry auth for registry2.example.com', async () => {
+    const auth = await getContainerRegistryAuth('registry2.example.com/test/test:latest');
+
+    expect(auth.username).toEqual('test');
+    expect(auth.password).toEqual('test2');
+});
+
+test('Pull image from private registry without auth', async () => {
+    process.env.EXPECT_ERROR = 'true';
+    try {
+        await pullImage('registry.gitlab.com/timokoessler/busybox:latest');
+        expect(false).toBeTruthy();
+    } catch (error) {
+        // Expected error
+    }
+    process.env.EXPECT_ERROR = '';
+});
+
+if (process.env.GITLAB_AUTH_TOKEN) {
+    test('Pull image from private registry with auth', async () => {
+        await pullImage('registry.gitlab.com/timokoessler/busybox:latest', {
+            username: 'TimoKoessler',
+            password: process.env.GITLAB_AUTH_TOKEN!,
+        });
+    });
+}
