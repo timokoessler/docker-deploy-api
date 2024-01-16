ed8e884a8a13dc1813f23567db8aab760c6dcdf2
Timo Kössler <info@timokoessler.de>
2024-01-16 12:38:51 +0100

    Cors: Add Access-Control-Max-Age header

--- src/routes/cors.ts
+++ src/routes/cors.ts
@@ -4,6 +4,7 @@ export function handleCORS(req: express.Request, res: express.Response, next: ex
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEAD');
     res.header('Access-Control-Allow-Headers', 'x-deploy-token');
+    res.header('Access-Control-Max-Age', '86400');
 
     if ('options' === req.method?.toLowerCase()) {
         return res.sendStatus(204);
